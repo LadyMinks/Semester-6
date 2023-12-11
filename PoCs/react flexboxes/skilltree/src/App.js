bb4b9ddbc41848f1702c6528d5d10f50fab7d447
LadyMinks <minka-96@outlook.com>
2023-12-11 10:46:14 +0100

    actually reading from json file.

--- PoCs/react flexboxes/skilltree/src/App.js
+++ PoCs/react flexboxes/skilltree/src/App.js
@@ -6,67 +6,8 @@ import LearningGoal from "./LearningGoal";
 
 function App() {
 
-    // data.learningGoals = [];
-    // const learningGoals = data.learningGoals;
-
-    const learningGoals = [
-        {
-            "id": 1,
-            "name": "Analyse",
-            "skills": [
-                {
-                    "id": 1,
-                    "name": "Architectuur: Meerlaagse Architectuur"
-                },
-                {
-                    "id": 2,
-                    "name": "Domeinmodellen"
-                },
-                {
-                    "id": 2,
-                    "name": "Klassendiagrammen"
-                }
-            ]
-        },
-        {
-            "id": 2,
-            "name": "Git",
-            "skills": [
-                {
-                    "id": 1,
-                    "name": "Repos"
-                },
-                {
-                    "id": 2,
-                    "name": "Cloning"
-                },
-                {
-                    "id": 2,
-                    "name": "commits"
-                }
-            ]
-        },
-        {
-            "id": 3,
-            "name": "Professionele vaardigheden",
-            "skills": [
-                {
-                    "id": 1,
-                    "name": "Communicatie"
-                },
-                {
-                    "id": 2,
-                    "name": "Feedback"
-                },
-                {
-                    "id": 2,
-                    "name": "Leadership"
-                }
-            ]
-        },
-    ]
-
-
+    const learningGoals = data.learningGoals;
+    
     return (
         <>
             <div className="d-flex flex-column mb-3 Main-flex">
--- PoCs/react flexboxes/skilltree/src/LearningGoalList.js
+++ PoCs/react flexboxes/skilltree/src/LearningGoalList.js
@@ -1,7 +1,9 @@
 import LearningGoal from "./LearningGoal";
 import learningGoal from "./LearningGoal";
+import data from "./database.json";
 
 export default function LearningGoalList({learningGoals}){
+
     return (
         <section className="d-flex flex-row mb-3 Semester-flex">
             {learningGoals.map(learningGoal => <LearningGoal key={learningGoal.id} learningGoal = {learningGoal}/>)}
